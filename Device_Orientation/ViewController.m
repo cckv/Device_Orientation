0664cd8cd91db562f5c397956d57020ebdc75f7b
chengy <chengy@bairuitech.com>
2019-05-28 11:07:07 +0800

    Initial Commit

---
+++ Device_Orientation/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Device_Orientation
+//
+//  Created by bairuitech on 2019/5/28.
+//  Copyright © 2019年 bairuitech. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
